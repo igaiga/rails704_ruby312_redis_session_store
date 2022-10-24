df882fa239aef2d51cb6003a85566e2ec31b023b
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-10-24 17:17:33 +0900

    Add updating session expire_after time code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_session_on_redis
+  def set_session_on_redis
+    session[:foo] = 555
+    session.instance_variable_get(:@by).instance_variable_get(:@conn).instance_variable_get(:@options)[:expire_after] = "600 seconds"
+    # binding.break
+    # @by @default_options にも入ってるのでそちらも書き換えた方が良さそう
+    # ただ、@by　@conn @options だけでも意図通り動いている
+    # TODO: セットする口がないかコード調べてみる
+    # ActionPack ActionDispatch Request session.rbのコードで良いと思うが、ぱっと見ではすぐには見つからなかった
+    # Rails704Ruby312RedisSessionStore::Application.config.session_store から追ってもいいかも
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,5 @@
 # For redis-actionpack gem
 Rails704Ruby312RedisSessionStore::Application.config.session_store :redis_store,
   key: "session_id",
-  expire_after: 8.hours
+  expire_after: 5.seconds #600.seconds # 8.hours
 # secure: true # ensures HTTP cookies are transferred from server to client on a secure (HTTPS) connection
-
